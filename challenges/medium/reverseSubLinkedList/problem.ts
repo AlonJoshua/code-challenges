5d1072417c5c33b86a303fdc0b289d44bdd4b4e3
Alon Joshua <alonjoshua@gmail.com>
2025-04-16 18:38:51 +0300

    reverse sublink list - clear problem

--- challenges/medium/reverseSubLinkedList/problem.ts
+++ challenges/medium/reverseSubLinkedList/problem.ts
@@ -32,43 +32,6 @@ export class ListNode {
 }
 
 // Uncomment the function the code inside, run 'npm run test' to test your code
-export function reverseBetween(head: ListNode | null, left: number, right: number): ListNode | null {
-  if (!head || left === right) return head;
+// export function reverseBetween(head: ListNode | null, left: number, right: number): ListNode | null {
 
-  let current: ListNode | null = head;
-  let prev: ListNode | null = null;
-
-  let pos = 1;
-  while (pos < left) {
-      prev = current;
-      current = current.next!;
-      pos++;
-  }
-
-  const beforeLeft = prev;
-  const leftNode = current;
-
-  let next = null;
-  prev = null;
-
-  while (pos <= right) {
-    if (current) {
-      next = current.next;
-      current.next = prev;
-      prev = current;
-      current = next
-      pos++;
-    }
-  }
-
-  if (beforeLeft) {
-      beforeLeft.next = prev;
-  } else {
-      head = prev;
-  }
-
-  leftNode.next = current;
-
-  return head;
-
-};
+// };
