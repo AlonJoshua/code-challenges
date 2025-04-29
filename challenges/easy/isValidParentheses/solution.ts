56d5aac7eb57dcd5a560ecc998639dceb3a8ad8e
Alon Joshua <alonjoshua@gmail.com>
2025-04-29 23:50:43 +0300

    add is valid parentheses problem - refactored solution

--- challenges/easy/isValidParentheses/solution.ts
+++ challenges/easy/isValidParentheses/solution.ts
@@ -41,5 +41,39 @@ export function isValidParentheses1(s: string): boolean {
       return false;
   }
 
+  // Time: O(n)
+  // Space: O(n)
   return true;
 };
+
+// A bit more refactored solution using 1 stack
+export function isValidParentheses2(s: string): boolean {
+
+    if (s.length === 0) {
+      return true;
+    }
+  
+    const stack = [];
+    
+    const parenthesesMap = {
+      ')': '(',
+      ']': '[',
+      '}': '{'
+    }
+  
+    for (let i = 0; i < s.length; i++) {
+      const currentParentheses = s[i] as keyof typeof parenthesesMap
+      if (!parenthesesMap[currentParentheses]) {
+        stack.push(s[i])
+      } else {
+        const leftParentheses = stack.pop();
+        if (parenthesesMap[currentParentheses] !== leftParentheses) {
+          return false;
+        }
+      }
+    }
+  
+    // Time: O(n)
+    // Space: O(n)
+    return stack.length === 0;
+  };
